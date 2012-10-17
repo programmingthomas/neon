400a06a4ab8f5f331fe6352f70ec9e710509ebab
TDimaline <thomas.dimaline@gmail.com>
2012-10-17 20:17:43 +0100

    second commit from laptop

---
+++ server/index.js
@@ -0,0 +1,10 @@
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./handler");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);

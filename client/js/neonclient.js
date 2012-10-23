1b9ee03b637c67d6ddcd03d9b04e0d5b0682b1d9
Programming Thomas <programmingthomas@gmail.com>
2012-10-23 17:39:01 +0100

    Adjusted Neonclient.js

--- client/js/neonclient.js
+++ client/js/neonclient.js
@@ -1,8 +1,11 @@
-function NeonClient()
+/*
+This file is the general client side JS bridge
+It can return stuff. Its pretty useful
+It does /api/ calls as documented on the wiki
+All functions in this file are prefixed with ns_
+*/
+
+function ns_login(username, password)
 {
-	//Property declerations
-
-	//Get property functions
-
-	//Set property functions
+	console.log("Going to attempt a login");
 }

b788ba8342eaacd61d8d05d025b5d903ba12a4eb
TDimaline <thomas.dimaline@gmail.com>
2012-10-26 14:48:36 +0100

    made code more.... readable

--- client/js/core.js
+++ client/js/core.js
@@ -1,32 +1,40 @@
 
-$(document).ready(function(){
+$(document).ready(function()
+{
 	setBackground();
 
-	$('#signup-realname').bind("keyup", function(){
+	$('#signup-realname').bind("keyup", function()
+	{
 		verify('realname');
 	});
 
-	$('#signup-username').bind("keyup", function(){
+	$('#signup-username').bind("keyup", function()
+	{
 		verify('username');
 	});
 
 });
 
-$(window).resize(function(){
+$(window).resize(function()
+{
 	setBackground();
 });
 
-setBackground = function(){
+setBackground = function()
+{
 	var widths = [1024, 1280, 1366, 1440, 1920, 1920];
 	var body_width = $("body").width();
 
-	if(body_width > 1920) { //too big
+	if(body_width > 1920) 
+	{ //too big
 		var image_path = "splashes/1920.jpg";
 	}
-	else if(body_width < 1024) { //too small
+	else if(body_width < 1024) 
+	{ //too small
 		var image_path = "splashes/1024.jpg";
 	}
-	else { //juuust right
+	else 
+	{ //juuust right
 		var image_width = roundUpWidth(widths, body_width)[1] + ".jpg"
 		var image_path = "splashes/" + image_width;
 	}
@@ -36,16 +44,19 @@ setBackground = function(){
 }
 
 
-var roundUpWidth = function(a, x) {
+var roundUpWidth = function(a, x) 
+{
     var lo, hi;
-    for (var i = a.length; i--;) {
+    for (var i = a.length; i--;) 
+    {
         if (a[i] <= x && (lo === undefined || lo < a[i])) lo = a[i];
         if (a[i] >= x && (hi === undefined || hi > a[i])) hi = a[i];
     };
     return [lo, hi];
 }
 
-var changeToDashboard = function(){
+var changeToDashboard = function()
+{
 	var dashboard_data = nc_dashboard(localStorage.username, localStorage.key);
 	alert("registered, going to dashboard");
 }
@@ -53,15 +64,19 @@ var changeToDashboard = function(){
 
 
 
-var verify = function(field){
+var verify = function(field)
+{
 	var value = $('#signup-'+field).val();
 	console.log(value);
 
-	if(field=="realname") {
-		if(value.length>=0) {
+	if(field=="realname") 
+	{
+		if(value.length>=0) 
+		{
 			var result = "Your <b>full</b> name";
 		}
-		else {
+		else 
+		{
 			var result = "It's good";
 		}
 	}
--- client/js/neonclient.js
+++ client/js/neonclient.js
@@ -5,167 +5,220 @@ It does /api/ calls as documented on the wiki
 All functions in this file are prefixed with nc_
 */
 
-function saveKey(data) {
-	if (data.request.successCode == 200) {
+function saveKey(data) 
+{
+	if (data.request.successCode == 200) 
+	{
 		localStorage.username = data.login.username;
 		localStorage.key = data.login.key;
 		changeToDashboard();
 	}
-	else {
+	else 
+	{
 		alert(data.request.message);
 	}
 }
 
 
-function nc_login(u, p) {
-	$.getJSON("/api/login/", {username:u, password:p}, function(data, status, xhr) {
+function nc_login(u, p) 
+{
+	$.getJSON("/api/login/", {username:u, password:p}, function(data, status, xhr) 
+	{
 		saveKey(data);
 	});
 }
 
-function nc_user(u, k, q, o) {
+function nc_user(u, k, q, o) 
+{
 	var html = "<section class='nc_user'>";
-	$.getJSON("/api/user/" + q, {username:u, key:k, offset:o}, function(data, status, xhr) {
+
+	$.getJSON("/api/user/" + q, {username:u, key:k, offset:o}, function(data, status, xhr) 
+	{
 			html += "<article>";
-			html += ("<span class='nc_user_id'>" + data.user.id.toString() + "</span>");
-			html += ("<span class='nc_user_username'>" + data.user.username + "</span>");			
-			html += ("<span class='nc_user_name'>" + data.user.name.toString() + "</span>");		
-			html += ("<img src='" + data.user.userImage + "' class='nc_user_userImage'></img>");
-			html += ("<ul class='nc_user_posts'>");
+
+			html += "<span class='nc_user_id'>" + data.user.id.toString() + "</span>";
+			html += "<span class='nc_user_username'>" + data.user.username + "</span>";			
+			html += "<span class='nc_user_name'>" + data.user.name.toString() + "</span>";		
+			html += "<img src='" + data.user.userImage + "' class='nc_user_userImage'></img>";
+
+			html += "<ul class='nc_user_posts'>";
 			for (var n = 0; n < data.user.posts.length; n++)
 			{
 				html += ("<li>" + data.user.posts[n].html + "</li>");
 			}
 			html += ("</ul>");
+
 			html += ("<ul class='nc_user_groups'>");
 			for (var n = 0; n < data.user.groups.length; n++)
 			{
 				html += ("<li>" + data.user.groups[n].html + "</li>");
 			}
-			html += ("</ul>");		
+			html += ("</ul>");
+
 			html += ("</article>");
+
 			html += ("</section>");
 			displayData(html);
 	});
 }
 
-function displayData(h){
+function displayData(h)
+{
 	console.log(h);
 }
 
-function nc_group(u, k, q, o) {
+function nc_group(u, k, q, o) 
+{
 	var html = "<section class='nc_group'>";
-	$.getJSON("/api/group/" + q, {username:u, key:k, offset:o}, function(data, status, xhr) {
+
+	$.getJSON("/api/group/" + q, {username:u, key:k, offset:o}, function(data, status, xhr) 
+	{
 			html += "<article>";
+
 			html += ("<span class='nc_group_id'>" + data.group.groupId.toString() + "</span>");
 			html += ("<span class='nc_group_groupname'>" + data.group.groupName + "</span>");	
+
 			html += ("<ul class='nc_group_groupcreator'>");
 			for (var n = 0; n < data.group.groupCreator.length; n++)
 			{
 				html += ("<li>" + data.user.groupCreator[n].html + "</li>");
 			}
-			html += ("</ul>");				
+			html += ("</ul>");
+
 			html += ("<span class='nc_group_groupcolor'>" + data.group.groupColor + "</span>");		
+
 			html += ("<ul class='nc_group_posts'>");
 			for (var n = 0; n < data.group.posts.length; n++)
 			{
 				html += ("<li>" + data.group.posts[n].html + "</li>");
 			}
 			html += ("</ul>");
+
 			html += ("</article>");
+
 			html += ("</section>");
 			displayData(html);
 	});
 }
 
-function nc_dashboard(u, k, o) {
+function nc_dashboard(u, k, o) 
+{
 	var html = "<section class='nc_dashboard'>";
-	$.getJSON("/api/dashboard/", {username:u, key:k, offset:o}, function(data, status, xhr) {
+
+	$.getJSON("/api/dashboard/", {username:u, key:k, offset:o}, function(data, status, xhr) 
+	{
 			html += ("<ul class='nc_dashboard_posts'>");
 			for (var n = 0; n < data.dashboard.posts.length; n++)
 			{
 				html += ("<li>" + data.dashboard.posts[n].html + "</li>");
 			}
 			html += ("</ul>");
+
 			html += ("</section>");
 			displayData(html);
 	});
 }
 
-function nc_dashboardSearch(u, k, o) {
+function nc_dashboardSearch(u, k, o) 
+{
 	var html = "<section class='nc_dashboardsearch'>";
-	$.getJSON("/api/dashboard/search/", {username:u, key:k, offset:o}, function(data, status, xhr) {
+
+	$.getJSON("/api/dashboard/search/", {username:u, key:k, offset:o}, function(data, status, xhr) 
+	{
 			html += ("<ul class='nc_dashboardsearch_posts'>");
 			for (var n = 0; n < data.dashboard.posts.length; n++)
 			{
 				html += ("<li>" + data.dashboard.posts[n].html + "</li>");
 			}
 			html += ("</ul>");
+
 			html += ("</section>");
 			displayData(html);
 	});
 }
 
-function nc_post(u, k, g, c, r) {
+function nc_post(u, k, g, c, r) 
+{
 	var html = "<section class='nc_post'>";	
-	$.getJSON("/api/post/", {username:u, key:k, groupId:g, content:c, responding:r}, function(data, status, xhr) {
+	$.getJSON("/api/post/", {username:u, key:k, groupId:g, content:c, responding:r}, function(data, status, xhr) 
+	{
 		html += "<span class='nc_post_id'>" + data.id + "</span>";
+
 		html += "</section";
 		displayData(html);
 	});
 }
 
-function nc_addNoteToPost(u, k, o, i) {
+function nc_addNoteToPost(u, k, o, i) 
+{
 	var html = "<section class='nc_addnotetopost'>";
-	$.getJSON("/api/post/", {username:u, key:k, operation:o, id:i}, function(data, status, xhr) {
+
+	$.getJSON("/api/post/", {username:u, key:k, operation:o, id:i}, function(data, status, xhr) 
+	{
 		html += "<span class='nc_addnotetopost_likes'>" + data.likes + "</span>";
 		html += "<span class='nc_addnotetopost_dislikes'>" + data.dislikes + "</span>";
-		html += "<span class='nc_addnotetopost_repostid'>" + data.repostId + "</span>";				
+		html += "<span class='nc_addnotetopost_repostid'>" + data.repostId + "</span>";			
+
 		html += "</section>";
 		displayData(html);
 	});
 }
 
-function nc_searchUsers(u, k, q, o) {
+function nc_searchUsers(u, k, q, o) 
+{
 	var html = "<section class='nc_searchusers'>";
-	$.getJSON("/api/searchusers/", {username:u, key:k, q:q, offset:o}, function(data, status, xhr) {
+
+	$.getJSON("/api/searchusers/", {username:u, key:k, q:q, offset:o}, function(data, status, xhr) 
+	{
 		html += ("<ul class='nc_searchusers_users'>");
+
 		for (var n = 0; n < data.users.length; n++)
 		{
 			html += ("<li>" + data.users[n].html + "</li>");
 		}
 		html += ("</ul>");
+
 		html += ("</section>");
 		displayData(html);
 	});
 }
 
-function nc_inbox(u, k, o, l, r) {
-	$.getJSON("/api/inbox/", {username:u, key:k, offset:o, length:l, readmessages:r}, function(data, status, xhr) {
+function nc_inbox(u, k, o, l, r) 
+{
+	$.getJSON("/api/inbox/", {username:u, key:k, offset:o, length:l, readmessages:r}, function(data, status, xhr) 
+	{
 		return data;
 	});
 }
 
-function nc_inboxCompose(u, k, r, c) {
-	$.getJSON("/api/inbox/compose/", {username:u, key:k, reciever:r, content:c}, function(data, status, xhr) {
+function nc_inboxCompose(u, k, r, c) 
+{
+	$.getJSON("/api/inbox/compose/", {username:u, key:k, reciever:r, content:c}, function(data, status, xhr) 
+	{
 		return data;
 	});
 }
 
-function nc_outbox(u, k, o, l, r) {
-	$.getJSON("/api/outbox/", {username:u, key:k, offset:o, length:l, readmessages:r}, function(data, status, xhr) {
+function nc_outbox(u, k, o, l, r) 
+{
+	$.getJSON("/api/outbox/", {username:u, key:k, offset:o, length:l, readmessages:r}, function(data, status, xhr) 
+	{
 		return data;
 	});
 }
 
-function nc_inboxDelete(u, k, r, c) {
-	$.getJSON("/api/inbox/delete/", {username:u, key:k, reciever:r, content:c}, function(data, status, xhr) {
+function nc_inboxDelete(u, k, r, c) 
+{
+	$.getJSON("/api/inbox/delete/", {username:u, key:k, reciever:r, content:c}, function(data, status, xhr) 
+	{
 		return data;
 	});
 }
 
-function nc_register(u, p, n) {
-	$.getJSON("/api/register/", {username:u, password:p, name:n}, function(data, status, xhr) {
+function nc_register(u, p, n) 
+{
+	$.getJSON("/api/register/", {username:u, password:p, name:n}, function(data, status, xhr) 
+	{
 		saveKey(data);
 	});
 }

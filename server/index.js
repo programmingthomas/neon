b88c602ec3697994815655f4d65d7c719e3a00ff
Programming Thomas <programmingthomas@gmail.com>
2013-05-27 21:24:07 +0100

    Removed old Node code
    I'm still rewriting api.js, hence why that is staying.

--- server/index.js
+++
@@ -1,7 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./handler");
-var log = require("./logger");
-
-log.i("index.js", "Starting server");
-server.start(router.route);
